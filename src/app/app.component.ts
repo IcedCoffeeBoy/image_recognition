3209fe9f9c924628f9aaf75f680a10fd35a0e7ea
IcedCoffeeBoy <lee.mingliang@outlook.com>
2019-08-12 11:02:57 +0800

    my first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,42 @@
-import { Component } from '@angular/core';
+import { ImageClassifierService } from './image-classifier.service';
+import { Component, OnInit, ViewChild, ElementRef } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
+  @ViewChild('imgOutput', { static: false }) imgRef: ElementRef;
+  loading = true;
   title = 'image-recognition';
+  imageInput;
+
+  constructor(
+    private classifierService: ImageClassifierService
+  ) { }
+
+  ngOnInit() {
+    this.classifierService.initClassifier();
+    this.classifierService.ready.subscribe((res) => {
+      this.loading = false;
+    });
+  }
+
+  processFile(files) {
+    const reader = new FileReader();
+    reader.readAsDataURL(files[0]);
+    reader.onload = (_event) => {
+      this.imageInput = reader.result;
+    }
+  }
+
+  async onPredict() {
+    // this.image = await this.camera.capturePhoto();
+    const predictions = await this.classifierService.classify(this.imgRef.nativeElement);
+    console.log(predictions);
+  }
+
 }
+
+
---
+++ src/app/image-classifier.service.ts
@@ -0,0 +1,57 @@
+import { Injectable, ComponentFactoryResolver } from '@angular/core';
+import * as tf from '@tensorflow/tfjs';
+import { ReplaySubject } from 'rxjs';
+import { IMAGENET_CLASSES } from '../assets/imagenet-classes';
+
+
+@Injectable({
+  providedIn: 'root'
+})
+export class ImageClassifierService {
+  ready = new ReplaySubject();
+  model;
+
+  constructor() {
+  }
+
+  async initClassifier() {
+    const modelUrl = '../assets/model.json';
+    // const handler = tfn.io.fileSystem('../assets/model.json');
+    this.model = await tf.loadGraphModel(modelUrl);
+    this.ready.next(true);
+  }
+
+  // preprocess the image to be mobilenet friendly
+  private preprocessImage(image) {
+    // resize the input image to mobilenet's target size of (224, 224)
+    let tensor;
+    try {
+      tensor = tf.browser.fromPixels(image)
+        .resizeNearestNeighbor([224, 224])
+        .toFloat();
+    } catch (err) {
+      console.log(err);
+    }
+    // if model is mobilenet, feature scale tensor image to range [-1, 1]
+    const offset = tf.scalar(127.5);
+    return tensor.sub(offset)
+      .div(offset)
+      .expandDims();
+  }
+
+  async classify(image) {
+    const tensor = this.preprocessImage(image);
+    const predictions = await this.model.predict(tensor).data();
+    // get the model's prediction results
+    let results = Array.from(predictions)
+      .map((p, i) => {
+        return {
+          probability: p,
+          className: IMAGENET_CLASSES[i]
+        };
+      }).sort((a: any, b: any) => {
+        return b.probability - a.probability;
+      }).slice(0, 5);
+    return results;
+  }
+}
